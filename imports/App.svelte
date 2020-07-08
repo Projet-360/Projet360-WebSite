039929655c8086ac136212a5c020efff041c6a09
Pierre <projet.troiscentsoixante@gmail.com>
2020-07-08 14:13:41 +0200

    Svelte

---
+++ imports/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte'
+  import jQuery from '../node_modules/jquery/dist/jquery.min';
+  import gsap from "gsap";
+  
+  import Center from './Center.svelte'
+  import Down   from './Down.svelte'
+  import Up     from './Up.svelte'
+  import Right  from './Right.svelte'
+  import Left   from './Left.svelte'
+
+  onMount(async () => {
+    if (!window.jQuery) window.jQuery = jQuery
+    window.jQuery = jQuery;
+    await import('../public/navigation.js')
+  })
+</script>
+
+<main role="main">
+
+    <Center/>
+    <Up/>
+    <Right/>
+    <Left/>
+    <Down/>
+
+</main>
--- public/navigation.js
+++ public/navigation.js
@@ -1,26 +1,26 @@
 //CHANGEMENTS DES COULEURS sur HTML POUR SECTIONS
-$(".link-up").click(function () {
-    $("main").css({
+jQuery(".link-up").click(function () {
+    jQuery("main").css({
         backgroundColor: '#04498b',
     });
 });
-$(".link-down").click(function () {
-    $("main").css({
+jQuery(".link-down").click(function () {
+    jQuery("main").css({
         backgroundColor: '#8b6804',
     });
 });
-$(".link-left").click(function () {
-    $("main").css({
+jQuery(".link-left").click(function () {
+    jQuery("main").css({
         backgroundColor: '#51048b',
     });
 });
-$(".link-right").click(function () {
-    $("main").css({
+jQuery(".link-right").click(function () {
+    jQuery("main").css({
         backgroundColor: '#048b34',
     });
 });
-$(".link-center").click(function () {
-    $("main").css({
+jQuery(".link-center").click(function () {
+    jQuery("main").css({
         backgroundColor: '#180330',
     });
 });
@@ -28,35 +28,35 @@ $(".link-center").click(function () {
 
 
 // PLACEMENT DE DEPART
-$(".section.image").css({
+jQuery(".section.image").css({
     top: '-100vh',
     right: '0vw',
     bottom: '100vh',
     left: '0vw',
 });
 
-$(".section.video").css({
+jQuery(".section.video").css({
     top: '100vh',
     right: '0vw',
     bottom: '-100vh',
     left: '0vw',
 });
 
-$(".section.audio").css({
+jQuery(".section.audio").css({
     top: '0vh',
     right: '100vw',
     bottom: '0vh',
     left: '-100vw',
 });
 
-$(".section.web").css({
+jQuery(".section.web").css({
     top: '0vh',
     right: '-100vw',
     bottom: '0vh',
     left: '100vw',
 });
 
-$(".section-center").css({
+jQuery(".section-center").css({
     top: '0vh',
     right: '0vw',
     bottom: '0vh',
@@ -65,186 +65,186 @@ $(".section-center").css({
 // FIN PLACEMENT DE DEPART
 
 // BUTTON RETURN
-$(".link-center").click(function () {
-    $(".section.image").animate({
+jQuery(".link-center").click(function () {
+    jQuery(".section.image").animate({
         top: '-100vh',
         right: '0vw',
         bottom: '100vh',
         left: '0vw',
     }, 900);
-    $(".section.video").animate({
+    jQuery(".section.video").animate({
         top: '100vh',
         right: '0vw',
         bottom: '-100vh',
         left: '0vw',
     }, 900);
-    $(".section-center").animate({
+    jQuery(".section-center").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $(".section.web").animate({
+    jQuery(".section.web").animate({
         top: '0vh',
         right: '-100vw',
         bottom: '0vh',
         left: '100vw',
     }, 900);
-    $(".section.audio").animate({
+    jQuery(".section.audio").animate({
         top: '0vh',
         right: '100vw',
         bottom: '0vh',
         left: '-100vw',
     }, 900);
-    $("li.return").animate({
+    jQuery("li.return").animate({
         opacity: '0',
     }, 500);
 });
 
 // BUTTON HAUT
-$(".link-up").click(function () {
-    $(".section.image").animate({
+jQuery(".link-up").click(function () {
+    jQuery(".section.image").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $(".section.video").animate({
+    jQuery(".section.video").animate({
         top: '200vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $(".section-center").animate({
+    jQuery(".section-center").animate({
         top: '100vh',
         right: '0vw',
         bottom: '-100vh',
         left: '0vw',
     }, 900);
-    $(".section.web").animate({
+    jQuery(".section.web").animate({
         top: '100vh',
         right: '100vw',
         bottom: '0vh',
         left: '100vw',
     }, 900);
-    $(".section.audio").animate({
+    jQuery(".section.audio").animate({
         top: '100vh',
         bottom: '-100vh',
         right: '100vw',
         left: '-100vw',
     }, 900);
-    $("li.return").animate({
+    jQuery("li.return").animate({
         opacity: '1',
     }, 500);
 });
 
 // BUTTON BAS
-$(".link-down").click(function () {
-    $(".section.image").animate({
+jQuery(".link-down").click(function () {
+    jQuery(".section.image").animate({
         top: '-300vh',
         right: '0vw',
         bottom: '200vh',
         left: '0vw',
     }, 900);
-    $(".section.video").animate({
+    jQuery(".section.video").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $(".section-center").animate({
+    jQuery(".section-center").animate({
         top: '-100vh',
         right: '0vw',
         bottom: '100vh',
         left: '0vw',
     }, 900);
-    $(".section.web").animate({
+    jQuery(".section.web").animate({
         top: '-100vh',
         right: '-100vw',
         bottom: '100vh',
         left: '100vw',
     }, 900);
-    $(".section.audio").animate({
+    jQuery(".section.audio").animate({
         top: '-100vh',
         bottom: '100vh',
         right: '100vw',
         left: '-100vw',
     }, 900);
-    $("li.return").animate({
+    jQuery("li.return").animate({
         opacity: '1',
     }, 500);
 });
 
 // BUTTON DROIT
-$(".link-right").click(function () {
-    $(".section.image").animate({
+jQuery(".link-right").click(function () {
+    jQuery(".section.image").animate({
         top: '-100vh',
         right: '100vw',
         bottom: '100vh',
         left: '-100vw',
     }, 900);
-    $(".section.video").animate({
+    jQuery(".section.video").animate({
         top: '100vh',
         right: '100vw',
         bottom: '-100vh',
         left: '-100vw',
     }, 900);
-    $(".section-center").animate({
+    jQuery(".section-center").animate({
         top: '0vh',
         right: '100vw',
         bottom: '0vh',
         left: '-100vw',
     }, 900);
-    $(".section.web").animate({
+    jQuery(".section.web").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $(".section.audio").animate({
+    jQuery(".section.audio").animate({
         top: '0vh',
         bottom: '0vh',
         right: '200vw',
         left: '-300vw',
     }, 900);
-    $("li.return").animate({
+    jQuery("li.return").animate({
         opacity: '1',
     }, 500);
 });
 
 // BUTTON GAUCHE
-$(".link-left").click(function () {
-    $(".section.image").animate({
+jQuery(".link-left").click(function () {
+    jQuery(".section.image").animate({
         top: '-100vh',
         right: '-100vw',
         bottom: '100vh',
         left: '100vw',
     }, 900);
-    $(".section.video").animate({
+    jQuery(".section.video").animate({
         top: '100vh',
         right: '-100vw',
         bottom: '-100vh',
         left: '100vw',
     }, 900);
-    $(".section-center").animate({
+    jQuery(".section-center").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '100vw',
     }, 900);
-    $(".section.web").animate({
+    jQuery(".section.web").animate({
         top: '0vh',
         right: '300vw',
         bottom: '0vh',
         left: '200vw',
     }, 900);
-    $(".section.audio").animate({
+    jQuery(".section.audio").animate({
         top: '0vh',
         right: '0vw',
         bottom: '0vh',
         left: '0vw',
     }, 900);
-    $("li.return").animate({
+    jQuery("li.return").animate({
         opacity: '1',
     }, 500);
 });
